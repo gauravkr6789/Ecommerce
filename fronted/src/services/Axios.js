b3e9599f63160384256c1b481205724ff50e4556
gauravkr6789 <mailmegaurav8810@gmail.com>
2026-06-30 17:14:44 +0530

    axios

--- fronted/src/services/Axios.js
+++ fronted/src/services/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://ecommerce-zdg5.onrender.com/api/auth/login",
   withCredentials: true,
 });
 
